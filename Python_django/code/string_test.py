2956c80920e04c06375df28d03490a1ba8c7d7a2
uditmahato <uditmahato29271@gmail.com>
2022-06-07 13:21:05 +0545

    fixed bug

--- Python_django/code/string_test.py
+++ Python_django/code/string_test.py
@@ -11,8 +11,8 @@ print(a+b+c)
 
 #delete  first name
 del c
-print(c)
-
+# print(c)
+# c=' is awesome'
 
 #len
 print(len('is awesome'))
@@ -22,3 +22,46 @@ print('s' in a)
 
 #use of not while checking
 print('not s' in a)
+
+#upper and lower in string
+print(a.upper())
+print(a.lower())
+
+#capitalize
+print(a.capitalize())
+
+#replace
+print(a.replace('is','is not'))
+
+#decision tree algorithm
+d='aeroplane'
+print(d.split(' ')) #split string into list
+print(d.split(' ')[0]) #get first element of list
+
+
+#string strip
+a='is aero this planet        '     #strip space remover
+print(len(a))
+print(len(a.strip()))
+
+
+#string indexing
+print(a[0])
+print(a[1])
+
+#string slicing
+print(a[0:5])  #first element is inclusive and second is exclusive
+print(a[0:6])
+
+#string slicing with negative index
+print(a[-1])
+print(a[-2])
+
+#string slicing with step
+print(a[0:6:2])
+print(a[0:6:3])
+
+#string slicing with colon
+print(a[:])
+
+
