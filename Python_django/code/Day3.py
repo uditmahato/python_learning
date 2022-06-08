fe701c541a2e62ffa2c8fbfa1a7adea2525da70d
uditmahato <uditmahato29271@gmail.com>
2022-06-08 12:34:28 +0545

    added a python file for today

---
+++ Python_django/code/Day3.py
@@ -0,0 +1,56 @@
+#Type casting
+
+#str to int
+print(int("10"))
+print(int("11"))
+
+#str to float
+print(float("10"))
+print(float("10.5"))
+
+#float to int
+print(int(10.5))
+print(int(10))
+
+#int to str
+print(str(10))
+print(str(10.5))
+
+#str to bool
+print(bool("True"))
+print(bool("False"))
+print(bool(" "))
+
+#int to bool
+print(bool(10))
+print(bool(0))
+print(bool(-1))
+
+#List
+#list is a collection of items
+#list is mutable
+#list is ordered
+#list is indexed
+#list is iterable
+#list is sequence
+
+#list creation
+d=[1,'a',2.5,'b',True]
+
+
+#list index
+print(d[0])
+print(d[1])
+print(d[2])
+
+#list slicing
+print(d[0:3])
+
+#list slicing with negative index
+print(d[-1])
+print(d[-2])
+
+#list slicing with step
+print(d[0:6:2])
+print(d[0:6:3])
+
