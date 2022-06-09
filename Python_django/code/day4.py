215afdab45a3bcb241ebf166ff754c1af04879bb
uditmahato <uditmahato29271@gmail.com>
2022-06-09 22:17:35 +0545

    hetrogenious tuple

--- Python_django/code/day4.py
+++ Python_django/code/day4.py
@@ -2,6 +2,10 @@
 tuple = (1,2,3,4,5,6,7,8,9,10)
 print(tuple)
 
+# heterogenious tuple
+heterogenious_tuple = (1,2,3,4,5,6,7,8,9,10,"a","b","c","d","e","f","g","h","i","j")
+print(heterogenious_tuple)
+
 
 #tuple properties
 
