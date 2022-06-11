8289f0096585ae85cbce10c7c865810484780d78
uditmahato <uditmahato29271@gmail.com>
2022-06-11 23:45:08 +0545

    added set properties

---
+++ Python_django/code/sets.py
@@ -0,0 +1,14 @@
+#Sets is    
+#A set is a collection which is unordered and unindexed. In Python sets are written with curly brackets.
+
+#set properties
+#------------------
+
+
+#set is mutable
+#set is unordered
+#set is indexed
+#set is iterable
+#set is sequence
+# set is heterogenous
+
