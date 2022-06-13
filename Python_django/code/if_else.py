f30fabbec3ce2bf6d446ce6e2b8be72f31480271
uditmahato <uditmahato29271@gmail.com>
2022-06-13 12:56:27 +0545

    aded elif statement

--- Python_django/code/if_else.py
+++ Python_django/code/if_else.py
@@ -7,10 +7,25 @@
 # else:
 #     statement
 
+from multiprocessing.sharedctypes import Value
+
+
 salary = int(input("Enter your salary: "))
 if salary > 10000:
     print("You are a rich person")
 else:
     print("You are not a rich person")
 
+
+#elif
+# elif is used to execute one of more statements based on a condition.
+# elif is used to execute one of more statements based on a condition.  It is used after if.
+
+Value = int(input("Enter your value: "))
+if Value > 0:
+    print("Value is positive")
+elif Value < 0:
+    print("Value is negative")
+else:
+    print("Value is zero")
     
