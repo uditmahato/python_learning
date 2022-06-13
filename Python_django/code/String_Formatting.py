ca5929b8bd9bd73d2be665e90a11abbb270015e2
uditmahato <uditmahato29271@gmail.com>
2022-06-13 12:44:09 +0545

    another way to format string added

--- Python_django/code/String_Formatting.py
+++ Python_django/code/String_Formatting.py
@@ -4,3 +4,9 @@
 name = "John"
 age = 23
 print("Hello %s. You are %d years old." % (name, age))
+
+# string formatting using curly braces
+print(f"Hello {name} .You are {age} years old")
+
+#another way using .format
+print("Hello {} .You are {} years old".format(name,age))
---
+++ Python_django/code/if_else.py
@@ -0,0 +1,3 @@
+# if-else example
+# if-else is a conditional statement in python.
+# if-else is used to execute one of two statements based on a condition.
