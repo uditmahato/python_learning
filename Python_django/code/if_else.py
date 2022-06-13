6ebadfd6404ff4a74948cbf80acf6d12b07c5b6c
uditmahato <uditmahato29271@gmail.com>
2022-06-13 13:19:44 +0545

    none

--- Python_django/code/if_else.py
+++ Python_django/code/if_else.py
@@ -1,70 +1,76 @@
-# if-else example
-# if-else is a conditional statement in python.
-# if-else is used to execute one of two statements based on a condition.
-#example
-# if condition:
-#     statement
+# # if-else example
+# # if-else is a conditional statement in python.
+# # if-else is used to execute one of two statements based on a condition.
+# #example
+# # if condition:
+# #     statement
+# # else:
+# #     statement
+
+# from multiprocessing.sharedctypes import Value
+
+
+# salary = int(input("Enter your salary: "))
+# if salary > 10000:
+#     print("You are a rich person")
 # else:
-#     statement
-
-from multiprocessing.sharedctypes import Value
+#     print("You are not a rich person")
 
 
-salary = int(input("Enter your salary: "))
-if salary > 10000:
-    print("You are a rich person")
-else:
-    print("You are not a rich person")
+# #elif
+# # elif is used to execute one of more statements based on a condition.
+# # elif is used to execute one of more statements based on a condition.  It is used after if.
+
+# Value = int(input("Enter your value: "))
+# if Value > 0:
+#     print("Value is positive")
+# elif Value < 0:
+#     print("Value is negative")
+# else:
+#     print("Value is zero")
+
+# # if else boolean
+# # if else boolean is used to execute one of two statements based on a condition.
+# # if else boolean is used to execute one of two statements based on a condition.  It is used after if.
+# # example
+# # if condition: 
+# #     statement
+# # else:
+# #     statement
+
+# #pass example
+# # pass is used to do nothing.
+# #code
+# x=False
+# if x==True:
+#     pass
+# else:
+#     print("This is not true")
 
 
-#elif
-# elif is used to execute one of more statements based on a condition.
-# elif is used to execute one of more statements based on a condition.  It is used after if.
+# #another example   
+# a=0
+# if a==0:
+#     print("a is zero")
+# elif a==1:
+#     print("a is one")
+# elif a==2:
+#     print("a is two")
+# elif a==3:
+#     print("a is three")
+# elif a==4:
+#     print("a is four")
+# else:
+#     print
 
-Value = int(input("Enter your value: "))
-if Value > 0:
-    print("Value is positive")
-elif Value < 0:
-    print("Value is negative")
-else:
-    print("Value is zero")
+# #comprehension example
 
-# if else boolean
-# if else boolean is used to execute one of two statements based on a condition.
-# if else boolean is used to execute one of two statements based on a condition.  It is used after if.
+# #example
+# a=10 if a>10 else 20
+# print(a)
+
+# none
+# none is used to represent the absence of a value.
 # example
-# if condition: 
-#     statement
-# else:
-#     statement
-
-#pass example
-# pass is used to do nothing.
-#code
-x=False
-if x==True:
-    pass
-else:
-    print("This is not true")
-
-
-#another example   
-a=0
-if a==0:
-    print("a is zero")
-elif a==1:
-    print("a is one")
-elif a==2:
-    print("a is two")
-elif a==3:
-    print("a is three")
-elif a==4:
-    print("a is four")
-else:
-    print
-
-#comprehension example
-
-#example
-a=10 if a>10 else 20
-print(a)
+x=None
+print(x)
