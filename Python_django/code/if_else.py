25226adf74fbcfe9f108d94e75c1d1f597f9f1ac
uditmahato <uditmahato29271@gmail.com>
2022-06-13 12:53:20 +0545

    example added

--- Python_django/code/if_else.py
+++ Python_django/code/if_else.py
@@ -1,3 +1,16 @@
 # if-else example
 # if-else is a conditional statement in python.
 # if-else is used to execute one of two statements based on a condition.
+#example
+# if condition:
+#     statement
+# else:
+#     statement
+
+salary = int(input("Enter your salary: "))
+if salary > 10000:
+    print("You are a rich person")
+else:
+    print("You are not a rich person")
+
+    
