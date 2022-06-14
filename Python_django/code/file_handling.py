6f06d88c8e741ed18a016f2f640a850f9ea53796
uditmahato <uditmahato29271@gmail.com>
2022-06-14 23:25:00 +0545

    added opening,creating and deleting  in file handling

--- Python_django/code/file_handling.py
+++ Python_django/code/file_handling.py
@@ -8,3 +8,23 @@ print(f.read())
 #closing a file
 f.close()
 
+#Reading a file line by line
+f = open("test.txt", "r")
+for line in f:
+    print(line)
+f.close()
+
+#writing to a file
+f = open("test.txt", "w")
+f.write("Hello World")
+f.close()
+
+#creating a file
+f = open("true.txt", "x")
+f.close()
+
+# deleting a file
+import os
+os.remove("test.txt")
+
+
