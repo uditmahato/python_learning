d9dd644deebcd3ea9428c2771d844f78ded06d6f
uditmahato <uditmahato29271@gmail.com>
2022-06-14 23:21:18 +0545

    added reading in file handling

---
+++ Python_django/code/file_handling.py
@@ -0,0 +1,10 @@
+# what is file handling in python?
+# file handling is the process of opening, reading, writing and closing files.
+#
+#opening a file
+f = open("test.txt", "r")
+#reading a file
+print(f.read())
+#closing a file
+f.close()
+
