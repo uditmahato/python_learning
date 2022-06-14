a48f2d683a1a42adeae636fb60cf9ce4448b3d8f
uditmahato <uditmahato29271@gmail.com>
2022-06-14 23:12:44 +0545

    removed an object

--- Python_django/code/class_marks.py
+++ Python_django/code/class_marks.py
@@ -1,7 +1,4 @@
 #dictionary student name and marks
-from pickletools import markobject
-
-
 student_marks = {'Mary': 93, 'Bob': 87, 'John': 80, 'Tom': 60,'Bharat':50,'raju':40,'siva':30,'krishna':98}
 std_name=input('Enter the name of students to check their marks: ')
 mark=student_marks[std_name]
