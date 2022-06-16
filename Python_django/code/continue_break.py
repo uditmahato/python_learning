6aebb5f644494178b82436c81328e9ba6e839a2e
uditmahato <uditmahato29271@gmail.com>
2022-06-16 13:14:58 +0545

    continue

--- Python_django/code/continue_break.py
+++ Python_django/code/continue_break.py
@@ -6,3 +6,12 @@ for i in range(10):
         break
     print(i)
 print("Done")
+
+#continue in python
+#continue is used to skip the current iteration and continue with the next iteration.
+# continue example
+for i in range(10):
+    if i == 5:
+        continue
+    print(i)
+print("Done")
