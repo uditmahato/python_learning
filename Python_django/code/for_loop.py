4ff2a51ab0e8837c9d16963f0989408f7d3bebe4
uditmahato <uditmahato29271@gmail.com>
2022-06-16 12:47:40 +0545

    -upacking in for loop of dictionary

--- Python_django/code/for_loop.py
+++ Python_django/code/for_loop.py
@@ -3,6 +3,15 @@ for i in range(10):
     print(i)
 print("Done")
 
+# for in dictionary
+d = {"a": 1, "b": 2, "c": 3}
+for key in d:
+    print(key)
+    
+for value in d.values():
+    print(value)
 
+for key, value in d.items():
+    print(key, value)
 
 
