693566252f7e803b9662ab54e57f58c30ea916ca
uditmahato <uditmahato29271@gmail.com>
2022-06-21 12:17:05 +0545

    added syntax error,logical error,run time error

---
+++ Python_django/code/error_handling.py
@@ -0,0 +1,19 @@
+#syntactical error
+
+# print("Hello World)
+
+
+#logcal error runs th code but do not give expected output
+
+#run time error
+while True:
+    try:
+        x = int(input("Please enter a number: "))
+        print(x/0)
+        break
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
+
+        #run time error results sudden termination of program
+        
+
