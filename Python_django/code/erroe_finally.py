4db3b4c54f82385345a655e6e9e1bd3a2ba20ab8
uditmahato <uditmahato29271@gmail.com>
2022-06-21 13:11:45 +0545

    added notes

---
+++ Python_django/code/erroe_finally.py
@@ -0,0 +1,17 @@
+# finally in exception
+try:
+    print('try...')
+    r = 10 / int('2')
+    print('result:', r)
+except ValueError as e:
+    print('ValueError:', e)
+except ZeroDivisionError as e:
+    print('ZeroDivisionError:', e)
+else:
+    print('no error!')
+finally:
+    print('finally...')
+print('END')
+
+#try finally
+#except else finally
