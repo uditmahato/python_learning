378a7920292ff3aa1a7ea5d79c999f8e70f83f7f
uditmahato <uditmahato29271@gmail.com>
2022-06-21 12:21:33 +0545

    try and except

--- Python_django/code/error_handling.py
+++ Python_django/code/error_handling.py
@@ -6,14 +6,25 @@
 #logcal error runs th code but do not give expected output
 
 #run time error
-while True:
-    try:
-        x = int(input("Please enter a number: "))
-        print(x/0)
-        break
-    except ValueError:
-        print("Oops!  That was no valid number.  Try again...")
+# while True:
+#     try:
+#         x = int(input("Please enter a number: "))
+#         print(x/0)
+#         break
+#     except ValueError:
+#         print("Oops!  That was no valid number.  Try again...")
 
-        #run time error results sudden termination of program
-        
+#         #run time error results sudden termination of program
+#         # run time error is called exception
+
+
+
+try:
+    #risky code
+    #code that can throw error
+    pass
+except:
+    pass
+
+
 
