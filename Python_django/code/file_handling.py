06147453513637857f26356e114cb0c38fdc304f
uditmahato <uditmahato29271@gmail.com>
2022-06-22 12:30:50 +0545

    commented

--- Python_django/code/file_handling.py
+++ Python_django/code/file_handling.py
@@ -5,26 +5,29 @@
 f = open("test.txt", "r")
 #reading a file
 print(f.read())
-#closing a file
-f.close()
 
-#Reading a file line by line
-f = open("test.txt", "r")
-for line in f:
-    print(line)
-f.close()
+print(f.read(5))
+print(f.readlines())
+# #closing a file
+# f.close()
 
-#writing to a file
-f = open("test.txt", "w")
-f.write("Hello World")
-f.close()
+# #Reading a file line by line
+# f = open("test.txt", "r")
+# for line in f:
+#     print(line)
+# f.close()
 
-#creating a file
-f = open("true.txt", "x")
-f.close()
+# #writing to a file
+# f = open("test.txt", "w")
+# f.write("Hello World")
+# f.close()
 
-# deleting a file
-import os
-os.remove("test.txt")
+# #creating a file
+# f = open("true.txt", "x")
+# f.close()
+
+# # deleting a file
+# import os
+# os.remove("test.txt")
 
 
