e735ea7bbc6676e6dd090666d1a5308365fee47e
uditmahato <uditmahato29271@gmail.com>
2022-06-30 12:29:05 +0545

    added value

---
+++ Python_django/code/oop/Sunway_Student.py
@@ -0,0 +1,31 @@
+class Sunway_Student:
+    __school_name='Sunway'
+    def __init__(self,name,age):
+        self.__name=name
+        self.__age=age
+    @classmethod
+    def get_school_name(cls):
+        return cls.__school_name
+    @classmethod
+    def set_school_name(cls,name):
+        cls.__school_name=name
+    @staticmethod
+    def get_school_address():
+        return 'No.1, Sunway Road, Sunway City'
+    def get_student_details(self):
+        return 'Name: '+self.name+' Age: '+str(self.age)
+
+    def get_age(self):
+        return self.__age
+    def set_age(self,new_age):
+        if new_age>0:
+            self.__age=new_age
+        else:
+            print('Invalid age')
+
+    
+s=Sunway_Student('Umesh',25)
+print(s.get_school_name())
+
+s.set_age(20)
+print(s.get_age())
