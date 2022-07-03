d662503551660d6e06bccf26fb616f69eff0ab26
uditmahato <uditmahato29271@gmail.com>
2022-07-03 23:22:26 +0545

    printed account created

--- Python_django/code/oop/bank_app.py
+++ Python_django/code/oop/bank_app.py
@@ -1,11 +1,11 @@
 class Bank_Account:
-    def __init__(self,first_name,last_name,balance):
+    def __init__(self,first_name,last_name):
         self.__first_name=first_name
         self.__last_name=last_name
         self.__balance=0
-  
+
     def check_balance(self):
-        print(f'Your balance is {Self.__balance}')
+        print(f'Your balance is {self.__balance}')
     def deposit_balance(self,deposit_amt):
         self.__balance+=deposit_amt
     def withdraw_balance(self,withdrawl_amt):
@@ -21,24 +21,25 @@ class Bank_Account:
         print('Tihar Holiday')
 user_account=None
 while True:
-    user_choice=input('Enter 1 to open bank account.\nEnter 2 to  check balance..\n Enter 3 to deposit balance. \n Enter 4 to withdraw balance.\n Enter 5 to Get interest Rate.\n Enter 6 to change interest rate. \n  Enter 7 to check holiday ')
-    if user_choice ==1:
+    user_choice=input('Enter 1 to open bank account.\nEnter 2 to  check balance.\nEnter 3 to deposit balance.\nEnter 4 to withdraw balance.\nEnter 5 to Get interest Rate.\nEnter 6 to change interest rate. \nEnter 7 to check holiday.\n')
+    if user_choice =='1':
         fn=input('Enter First Name : ')
         ln=input('Enter Last Name : ')
         user_account=Bank_Account(fn,ln)
-    elif user_choice==2:
+        print('Congratulations! You have opened a bank account with us.')
+    elif user_choice=='2':
         user_account.check_balance()
-    elif user_choice==3:
+    elif user_choice=='3':
         balance=input('Enter deposit amount:  ')
         user_account.deposit_balance(balance)
-    elif user_choice==4:
+    elif user_choice=='4':
         withdraw=input('Enter the withdrawl amount : ')
         user_account.withdraw_balance(withdraw)
-    elif user_choice==5:
+    elif user_choice=='5':
         user_account.get_interest_rate()
-    elif user_choice==6:
+    elif user_choice=='6':
         pass
-    elif user_choice==7:
+    elif user_choice=='7':
         Bank_Account.print_holiday_list()
     else:
         print("Print invalid input")
